3878ce24236f57c964bca59a85311bd789dcca80
Jofre Tomas <jofretomas9@gmail.com>
2024-12-10 19:39:29 -0300

    AUTOMATIZAÇÃO DO LOGIN DO USER

--- app.py
+++ app.py
@@ -4,12 +4,21 @@ from time import sleep
 
 drive = webdriver.Chrome()
 drive.get('https://consulta-empresa.netlify.app/')
+sleep(4)
 
-login = drive.find_element(By.XPATH, "//input[@id='username']")
+user = drive.find_element(By.XPATH, "//input[@id='username']")
 sleep(1)
-senha = drive.find_element(By.XPATH, "//input[@id='password']")
+user.click()
+user.send_keys('jhonatan')
+
+passaword = drive.find_element(By.XPATH, "//input[@id='password']")
 sleep(1)
-entra = drive.find_element(By.XPATH, "//class[@id='btn btn-primary btn-lg']")
+passaword.click()
+passaword.send_keys('12345678')
+
+button_login = drive.find_element(
+    By.XPATH, "//class[@id='btn btn-primary btn-lg']")
+button_login.click()
 sleep(1)
 
 input('aperte para entra')
